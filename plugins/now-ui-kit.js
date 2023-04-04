1f58432f11aca2abf378dc347f654e19b8b2cbc4
Anita-Kozlak <anita.kozlak@gmail.com>
2023-04-05 00:07:56 +0200

    remove unused components

--- plugins/now-ui-kit.js
+++ plugins/now-ui-kit.js
@@ -4,14 +4,12 @@ import lang from 'element-ui/lib/locale/lang/en';
 import locale from 'element-ui/lib/locale';
 import VueLazyload from 'vue-lazyload';
 import VueChatScroll from "vue-chat-scroll";
-// import ReadMore from 'vue-read-more';
 import Vue2TouchEvents from "vue2-touch-events";
 import Vuelidate from "vuelidate";
 locale.use(lang);
 
 export default () => {
   Vue.use(globalComponents);
-    // Vue.use(ReadMore);
   Vue.use(Vue2TouchEvents);
   Vue.use(Vuelidate);
   Vue.use(VueChatScroll);
